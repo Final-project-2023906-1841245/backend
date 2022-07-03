2d830e98061ee3db46c80944c482b4b41fcdcc53
Sebastian Caicedo <sdrivert@hotmail.com>
2022-07-02 22:56:08 -0500

    User simple verificacion added

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const {Pool} = require('pg')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 5000
+//Body-parser for post request:
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 // Database
+var mensaje = ""
 const config = {
   user: 'postgres',
   host: 'localhost',
@@ -11,22 +16,23 @@ const config = {
 }
 const pool = new Pool(config)
 
-const getUser = async() =>{
-  const ans = await pool.query('SELECT * FROM users WHERE user_phone=1231')
-  if(ans.rows.length == 0){
-    return "No se encontró el hp usuario, sapo"
+const getUser = async(values) =>{
+  const answer = await pool.query('SELECT * FROM users WHERE user_phone=$1 AND email=$2', values)
+  return answer
   }
-  else{
-    return "Sí se encontró crack"
+
+app.post("/user", async(req, res)=>{
+  var email = req.body.useremail
+  var phone = req.body.userphone
+  const values = [phone, email]
+  const answer = await getUser(values);
+  if(answer.rows.length == 0){
+    mensaje = "El usuario no existe"
+  }else{
+    mensaje = "El usuario está registrado"
   }
-}
-app.get("/user", async(req, res)=>{
-  const ans =await getUser();
-  res.send(ans)
+  res.send(mensaje);
 })
 app.listen(port, ()=>{
   console.log("The server is running on port 5000")
 })
-
-
-
