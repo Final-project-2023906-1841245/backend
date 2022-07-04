0c039a983f098b59cd021b0e95d480d911ffe276
Sebastian Caicedo <sdrivert@hotmail.com>
2022-07-04 15:24:49 -0500

    Insert user into database added

--- app.js
+++ app.js
@@ -19,20 +19,38 @@ const pool = new Pool(config)
 const getUser = async(values) =>{
   const answer = await pool.query('SELECT * FROM users WHERE user_phone=$1 AND email=$2', values)
   return answer
-  }
+}
 
-app.post("/user", async(req, res)=>{
+const insertUser = async(values) =>{
+  const answer = await pool.query('INSERT INTO users VALUES($1,$2,$3)', values)
+  return answer
+}
+
+app.post("/userlogin", async(req, res)=>{
   var email = req.body.useremail
   var phone = req.body.userphone
   const values = [phone, email]
   const answer = await getUser(values);
   if(answer.rows.length == 0){
-    mensaje = "El usuario no existe"
+    res.send(false)
   }else{
-    mensaje = "El usuario está registrado"
+    res.send(true)
   }
-  res.send(mensaje);
 })
+
+app.post("/usersignup", async(req, res)=>{
+  var name = req.body.username
+  var email = req.body.useremail
+  var phone = req.body.userphone
+  const values = [phone, name, email]
+  const searchUser = await getUser([phone, email]);
+  if(searchUser.rows.length == 0){
+    await insertUser(values)
+    res.send(true)
+  }else{
+    res.send(false)
+  }
+})
+
 app.listen(port, ()=>{
-  console.log("The server is running on port 5000")
-})
+  console.log("The server is running on port 5000") });
