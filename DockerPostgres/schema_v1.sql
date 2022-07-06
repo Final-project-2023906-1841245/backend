86fa3ba28e3e138a1d3b7eb38c7f9828fbd149d3
Sebastian Caicedo <sdrivert@hotmail.com>
2022-07-05 21:56:22 -0500

    Dockerfile for postgres image added

---
+++ DockerPostgres/schema_v1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE mande_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    TEMPLATE template0;
+
+\c mande_db
+
+CREATE TABLE usuario(
+	user_phone BIGINT PRIMARY KEY,
+  user_name TEXT UNIQUE NOT NULL,
+	email TEXT NOT NULL
+);
